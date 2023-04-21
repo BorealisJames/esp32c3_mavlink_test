e2816bd4d1a0956f107fedf687b5a65f722e7647
BorealisJames <james_halim@sutd.edu.sg>
2023-04-21 11:27:09 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# MavESP8266
+
+## Current Binary
+
+Download the current version (MAVLink V2) from here: [Firmware version 1.2.2](http://www.grubba.com/mavesp8266/firmware-1.2.2.bin)
+
+Download the legacy version (MAVLink V1) from here: [Firmware version 1.1.1](http://www.grubba.com/mavesp8266/firmware-1.1.1.bin)
+
+## ESP8266 WiFi Access Point and MavLink Bridge
+
+[![Join the chat at https://gitter.im/dogmaphobic/mavesp8266](https://badges.gitter.im/dogmaphobic/mavesp8266.svg)](https://gitter.im/dogmaphobic/mavesp8266?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+This was developed using a [NodeMCU v2 Dev Kit](http://www.seeedstudio.com/depot/NodeMCU-v2-Lua-based-ESP8266-development-kit-p-2415.html) as it conveniently provides a secondary UART for debugging. It has been tested with the ESP-01 shipped with the [PixRacer](https://pixhawk.org/modules/pixracer) and it is stable at 921600 baud.
+
+The build enviroment is based on [PlatformIO](http://platformio.org). Follow the instructions found here: http://platformio.org/#!/get-started (only tested on Mac OS) for installing it but skip the ```platform init``` step as this has already been done, modified and it is included in this repository. In summary:
+
+```
+brew install platformio
+git clone --recursive https://github.com/dogmaphobic/mavesp8266.git
+cd mavesp8266
+platformio run
+```
+
+When you run ```platformio run``` for the first time, it will download the toolchains and all necessary libraries automatically.
+
+### Useful commands: ESP32-WROOM
+
+* ```platformio run -e espwroom32 -t upload```
+
+### Useful commands: ESP12e
+
+* ```platformio run``` - process/build all targets
+* ```platformio run -e esp12e``` - process/build just the ESP12e target (the NodeMcu v2, Adafruit HUZZAH, etc.)
+* ```platformio run -e esp12e -t upload``` - build and upload firmware to embedded board
+* ```platformio run -t clean``` - clean project (remove compiled files)
+
+### Useful commands: ESP-01
+* ```platformio run``` - process/build all targets
+* ```platformio run -e esp01_1m``` - process/build just the ESP01 target with 1MB [Amazon](https://www.amazon.com/gp/product/B01EA3UJJ4)
+* ```platformio run -e esp01_1m -t upload``` - build and upload firmware to embedded board
+* ```platformio run -t clean``` - clean project (remove compiled files)
+
+
+
+The resulting image(s) can be found in the directory ```.pioenvs``` created during the build process.
+
+### MavLink Submodule
+
+The ```git clone --recursive``` above not only cloned the MavESP8266 repository but it also installed the dependent [MavLink](https://github.com/mavlink/c_library) sub-module. To upated the module (when needed), use the command:
+
+```git submodule update --init```
+
+### Wiring it up
+
+User level (as well as wiring) instructions can be found [here for px4](https://docs.px4.io/en/telemetry/esp8266_wifi_module.html) and [here for ArduPilot](http://ardupilot.org/copter/docs/common-esp8266-telemetry.html)
+
+* Resetting to Defaults: In case you change the parameters and get locked out of the module, all the parameters can be reset by bringing the GPIO02 pin low (Connect GPIO02 pin to GND pin). 
+
+Get the ESP-01 adapter board here on [Amazon](https://www.amazon.com/gp/product/B07Q17XJ36/); commonly called an "ESP01 programmer", this one has a little rocker switch on the side (UART side for serial TTL debugging by AT commands, PROG for firmware programming) and a yellow pin header which allows you plugin the ESP01 module without any wires. It defaults to 115200 and enumerates under Ubuntu as a ch341-uart converter. 
+
+
+### MavLink Protocol
+
+The MavESP8266 handles its own set of parameters and commands. Look at the [PARAMETERS](PARAMETERS.md) page for more information.
+
+### HTTP Protocol
+
+There are some preliminary URLs that can be used for checking the WiFi Bridge status as well as updating firmware and changing parameters. [You can find it here.](HTTP.md)
---
+++ src/main.cpp
@@ -0,0 +1,269 @@
+/****************************************************************************
+ *
+ * Copyright (c) 2015, 2016 Gus Grubba. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file main.cpp
+ * ESP8266 Wifi AP, MavLink UART/UDP Bridge
+ *
+ * @author Gus Grubba <mavlink@grubba.com>
+ */
+
+#include "mavesp8266.h"
+#include "mavesp8266_parameters.h"
+#include "mavesp8266_gcs.h"
+#include "mavesp8266_vehicle.h"
+#include "mavesp8266_httpd.h"
+#include "mavesp8266_component.h"
+
+#ifndef ARDUINO_ESP32_DEV
+#include <ESP8266mDNS.h>
+#else
+/* ESP32 */
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+#include <esp_event.h>
+#include <esp_event_loop.h>
+#include <esp_wifi.h>
+#include <esp_wifi_types.h>
+#include <esp_err.h>
+#include <mdns.h>
+#endif
+
+#define GPIO02  2
+
+//---------------------------------------------------------------------------------
+//-- HTTP Update Status
+class MavESP8266UpdateImp : public MavESP8266Update {
+public:
+    MavESP8266UpdateImp ()
+        : _isUpdating(false)
+    {
+
+    }
+    void updateStarted  ()
+    {
+        _isUpdating = true;
+    }
+    void updateCompleted()
+    {
+        //-- TODO
+    }
+    void updateError    ()
+    {
+        //-- TODO
+    }
+    bool isUpdating     () { return _isUpdating; }
+private:
+    bool _isUpdating;
+};
+
+
+
+//-- Singletons
+IPAddress               localIP;
+MavESP8266Component     Component;
+MavESP8266Parameters    Parameters;
+MavESP8266GCS           GCS;
+MavESP8266Vehicle       Vehicle;
+MavESP8266Httpd         updateServer;
+MavESP8266UpdateImp     updateStatus;
+MavESP8266Log           Logger;
+
+//---------------------------------------------------------------------------------
+//-- Accessors
+class MavESP8266WorldImp : public MavESP8266World {
+public:
+    MavESP8266Parameters*   getParameters   () { return &Parameters;    }
+    MavESP8266Component*    getComponent    () { return &Component;     }
+    MavESP8266Vehicle*      getVehicle      () { return &Vehicle;       }
+    MavESP8266GCS*          getGCS          () { return &GCS;           }
+    MavESP8266Log*          getLogger       () { return &Logger;        }
+};
+
+MavESP8266WorldImp      World;
+
+MavESP8266World* getWorld()
+{
+    return &World;
+}
+
+//---------------------------------------------------------------------------------
+//-- Wait for a DHCPD client
+void wait_for_client() {
+    DEBUG_LOG("Waiting for a client...\n");
+#ifdef ENABLE_DEBUG
+    int wcount = 0;
+#endif
+#ifdef ARDUINO_ESP32_DEV
+    uint8 client_count = WiFi.softAPgetStationNum();
+#else    
+    uint8 client_count = wifi_softap_get_station_num();
+#endif    
+    while (!client_count) {
+#ifdef ENABLE_DEBUG
+        Serial.print(".");
+        if(++wcount > 80) {
+            wcount = 0;
+            Serial.println();
+        }
+#endif
+        delay(1000);
+#ifdef ARDUINO_ESP32_DEV
+	client_count = WiFi.softAPgetStationNum();
+#else	
+        client_count = wifi_softap_get_station_num();
+#endif	
+    }
+    DEBUG_LOG("Got %d client(s)\n", client_count);
+}
+
+//---------------------------------------------------------------------------------
+//-- Reset all parameters whenever the reset gpio pin is active
+void reset_interrupt(){
+    Parameters.resetToDefaults();
+    Parameters.saveAllToEeprom();
+#ifdef ARDUINO_ESP32_DEV
+    ESP.restart();
+#else    
+    ESP.reset();
+#endif    
+}
+
+//---------------------------------------------------------------------------------
+//-- Set things up
+void setup() {
+    delay(1000);
+    Parameters.begin();
+#ifdef ENABLE_DEBUG
+    //   We only use it for non debug because GPIO02 is used as a serial
+    //   pin (TX) when debugging.
+#else
+    //-- Initialized GPIO02 (Used for "Reset To Factory")
+    pinMode(GPIO02, INPUT_PULLUP);
+    attachInterrupt(GPIO02, reset_interrupt, FALLING);
+#endif
+    Logger.begin(2048);
+
+    DEBUG_LOG("\nConfiguring access point...\n");
+    DEBUG_LOG("Free Sketch Space: %u\n", ESP.getFreeSketchSpace());
+
+    WiFi.disconnect(true);
+
+    if(Parameters.getWifiMode() == WIFI_MODE_STA){
+        //-- Connect to an existing network
+#ifdef ARDUINO_ESP32_DEV
+        WiFi.mode(WIFI_MODE_STA);      
+#else      
+        WiFi.mode(WIFI_STA);
+#endif	
+        // WiFi.config(Parameters.getWifiStaIP(), Parameters.getWifiStaGateway(), Parameters.getWifiStaSubnet(), 0U, 0U);
+        WiFi.config(Parameters.getWifiStaIP(), Parameters.getWifiStaGateway(), Parameters.getWifiStaSubnet());
+        WiFi.begin(Parameters.getWifiStaSsid(), Parameters.getWifiStaPassword());
+
+        //-- Wait a minute to connect
+        for(int i = 0; i < 120 && WiFi.status() != WL_CONNECTED; i++) {
+            #ifdef ENABLE_DEBUG
+            Serial.print(".");
+            #endif
+            delay(500);
+        }
+        if(WiFi.status() == WL_CONNECTED) {
+            localIP = WiFi.localIP();
+            WiFi.setAutoReconnect(true);
+        } else {
+            //-- Fall back to AP mode if no connection could be established
+            WiFi.disconnect(true);
+            Parameters.setWifiMode(WIFI_MODE_AP);
+        }
+    }
+
+    if(Parameters.getWifiMode() == WIFI_MODE_AP){
+        //-- Start AP
+#ifdef ARDUINO_ESP32_DEV
+	WiFi.mode(WIFI_MODE_AP);      /* Default to WPA2 */
+#else
+        WiFi.mode(WIFI_AP);
+        WiFi.encryptionType(AUTH_WPA2_PSK);	
+#endif	
+        WiFi.softAP(Parameters.getWifiSsid(), Parameters.getWifiPassword(), Parameters.getWifiChannel());
+        localIP = WiFi.softAPIP();
+        wait_for_client();
+    }
+    //-- Boost power to Max    
+#ifdef ARDUINO_ESP32_DEV
+    {
+         int8_t power;
+	 esp_wifi_get_max_tx_power(&power);
+	 esp_wifi_set_max_tx_power(power);
+    }
+#else
+    WiFi.setOutputPower(20.5);
+#endif    
+    char mdsnName[256];
+    sprintf(mdsnName, "MavEsp8266-%d",localIP[3]);
+#ifdef ARDUINO_ESP32_DEV
+    mdns_init();
+    mdns_service_add(NULL, "http", "tcp", 80, NULL, 0);
+#else    
+    MDNS.begin(mdsnName);
+    MDNS.addService("http", "tcp", 80);
+#endif    
+    //-- Initialize Comm Links
+    DEBUG_LOG("Start WiFi Bridge\n");
+    DEBUG_LOG("Local IP: %s\n", localIP.toString().c_str());
+
+    Parameters.setLocalIPAddress(localIP);
+    IPAddress gcs_ip(localIP);
+    //-- I'm getting bogus IP from the DHCP server. Broadcasting for now.
+    gcs_ip[3] = 255;
+    GCS.begin(&Vehicle, gcs_ip);
+    Vehicle.begin(&GCS);
+    //-- Initialize Update Server
+    updateServer.begin(&updateStatus);
+}
+
+//---------------------------------------------------------------------------------
+//-- Main Loop
+void loop() {
+    if(!updateStatus.isUpdating()) {
+        if (Component.inRawMode()) {
+            GCS.readMessageRaw();
+            delay(0);
+            Vehicle.readMessageRaw();
+
+        } else {
+            GCS.readMessage();
+            delay(0);
+            Vehicle.readMessage();
+        }
+    }
+    updateServer.checkUpdates();
+}
